f1fffe670f02a42334f5480d6efe66f0610ad8e5
Stablest <marlonlocci@hotmail.com>
2025-04-17 14:39:41 -0300

    fix: tailwind is now correctly parsing ts files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./src/app/**/*.{html, ts}"
+        "./src/app/**/*.html",
+        "./src/app/**/*.ts"
     ],
     theme: {
         extend: {
