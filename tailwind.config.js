4b71dde7854163e3862bec70ad8269e8835b9f48
Stablest <marlonlocci@hotmail.com>
2025-04-17 00:39:38 -0300

    chore: integrate tailwindcss into project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/app/**/*.{html, ts}"
+    ],
+    theme: {
+        extend: {
+            colors: {
+            }
+        },
+    },
+    plugins: [],
+}
+
